538f53e99278e5623647816ddf750b6851ebffb4
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-12-07 00:14:47 -0500

    Create an example project with schedules app
    Introduces some testable components. Includes a recurring schedule
    model and a utility to generate occurrences of the schedule's events.

---
+++ example/schedules/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SchedulesConfig(AppConfig):
+    """Recurring schedule calculation."""
+
+    name = "schedules"
---
+++ example/schedules/models.py
@@ -0,0 +1,43 @@
+from datetime import datetime, timedelta
+
+from django.db import models
+from django.utils import timezone
+
+
+class Schedule(models.Model):
+    """Recurring schedule."""
+
+    name = models.CharField(default="", max_length=128)
+    start = models.DateTimeField(default=timezone.now)
+    finish = models.DateTimeField(default=None, null=True)
+    every = models.DurationField(default=timedelta(hours=1))
+
+    def next_occurrence(self) -> datetime:
+        """Date and time for the next occurrence."""
+        occurrences = ScheduleIterator(self)
+        now = timezone.now()
+        while next_occurrence := next(occurrences):
+            if next_occurrence > now:
+                return next_occurrence
+
+    def __iter__(self):
+        return ScheduleIterator(self)
+
+
+class ScheduleIterator:
+    """Produce Schedule occurrences."""
+
+    def __init__(self, schedule: Schedule):
+        self._schedule = schedule
+        self._next_occurrence = schedule.start
+
+    def has_occurrences(self) -> bool:
+        """Any occurrences remaining before the finish, or ."""
+        if self._schedule.finish:
+            return self._next_occurrence < self._schedule.finish
+        return False
+
+    def __next__(self) -> datetime:
+        while self.has_occurrences():
+            yield self._next_occurrence
+            self._next_occurrence += self._schedule.every
