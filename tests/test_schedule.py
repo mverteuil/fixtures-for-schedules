7de7c9448a475463db4ec9f1305f7cfa04dfb00d
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-12-07 21:33:43 -0500

    Step 5

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -1,10 +1,9 @@
-from unittest import mock
+from django.utils import timezone
 
 from schedules.models import Schedule
 
 
 def test_default_start_date():
     """Should be now if not otherwise specified."""
-    with mock.patch("schedules.models.timezone") as mock_timezone:
-        schedule = Schedule()
-        assert schedule.start == mock_timezone.now()
+    start_field = Schedule._meta.get_field("start")
+    assert start_field.default is timezone.now
