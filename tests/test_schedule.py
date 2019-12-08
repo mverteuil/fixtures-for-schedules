0f06c47aefe435cdd66e2a965adcf24d8e132557
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-12-07 21:41:32 -0500

    Step 7

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -1,11 +1,16 @@
+from datetime import timedelta
+
 import pytest
 from django.utils import timezone
 
 from schedules.models import Schedule
 
 
-@pytest.mark.parametrize(("field_name", "expected"), (("start", timezone.now),))
-def test_default_start_date(field_name, expected):
-    """Should be now if not otherwise specified."""
+@pytest.mark.parametrize(
+    ("field_name", "expected"),
+    [("name", ""), ("start", timezone.now), ("finish", None), ("every", timedelta(hours=1)),],
+)
+def test_schedule_default_values(field_name, expected):
+    """Should have expected default value."""
     schedule_field = Schedule._meta.get_field(field_name)
     assert schedule_field.default == expected
