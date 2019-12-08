0f95ef3c7126dd82a3e70a524c47e3500642885e
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-12-07 21:19:41 -0500

    Step 1

---
+++ tests/test_schedule.py
@@ -0,0 +1,8 @@
+from django.utils import timezone
+from schedules.models import Schedule
+
+
+def test_schedule_defaults():
+    """Should have expected default values."""
+    schedule = Schedule()
+    assert schedule.start == timezone.now()
