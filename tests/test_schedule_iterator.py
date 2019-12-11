a1899bcba43a2466952202f043450ad2bdc861eb
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-12-10 21:36:01 -0500

    Step 13

--- tests/test_schedule_iterator.py
+++ tests/test_schedule_iterator.py
@@ -1,9 +1,17 @@
+from datetime import datetime, timedelta
+from unittest import mock
+
 import pytest
 
 from schedules.models import ScheduleIterator
 
 
-def test_initialize():
+@pytest.fixture(name="mock_schedule")
+def create_mock_schedule():
+    """Create a mock schedule with a start date and interval configured."""
+    return mock.Mock(name="Schedule", start=datetime(2000, 1, 2, 3, 4, 5, 6), every=timedelta(hours=1))
+
+
+def test_initialize(mock_schedule):
     """Should require a schedule."""
-    with pytest.raises(TypeError):
-        ScheduleIterator()
+    ScheduleIterator(mock_schedule)
